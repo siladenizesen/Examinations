ce6f363496dcf42311f6459d2563fe68675a2c6f
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-20 13:47:23 +0300

    Create file_read_write.c

---
+++ file_read_write.c
@@ -0,0 +1,21 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main(){
+    FILE *fp;
+    char ch;
+    fp = fopen("C:\\Users\\akifc\\Desktop\\test.txt","r");
+    if(fp == NULL){
+        printf("Dosya acilamadi");
+        exit(1);
+    }
+    while((ch = fgetc(fp)) != EOF){
+        printf("%c",ch);
+    }
+    fclose(fp);
+    return 0;
+}
+
+
